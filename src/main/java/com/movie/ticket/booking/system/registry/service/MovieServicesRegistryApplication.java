6b120ac5aa1533a31a6cc645978e9678c44774d8
unikdahal <unikdahal03@gmail.com>
2024-10-01 19:24:32 +0530

    Initial Project Setup

---
+++ src/main/java/com/movie/ticket/booking/system/registry/service/MovieServicesRegistryApplication.java
@@ -0,0 +1,15 @@
+package com.movie.ticket.booking.system.registry.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MovieServicesRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieServicesRegistryApplication.class, args);
+    }
+
+}
